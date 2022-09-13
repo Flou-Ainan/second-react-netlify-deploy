b3cec798a3a7f08bb897e217ea47731d4032bb57
Flou-Ainan <flouainan@gmail.com>
2022-09-13 20:42:42 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Deploy React App na Netlify - Desafio 13/09/2022 
         </p>
         <a
           className="App-link"
